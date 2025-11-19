60402028c7c258d83d8129eb16b02bdcefadcbe7
dmitriyneto <qima10021981@gmail.com>
2025-11-18 20:44:21 -0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+    public static void Main (String []args) {
+
+
+            int eater = 5; // сколько людей будет есть
+            int water = 3000;
+            int potatoes = 5;
+            int chicken = 6;
+            int spices = 10;
+
+
+
+
+        System.out.println("water");
+        System.out.println(("water / eaters") + "миллилитров (а) воды");
+        System.out.println(("potatoes / eaters") + "картофелин (а/ы)");
+        System.out.println(("chicken / eaters") + "куриных(ое) бёдер(ро)");
+        System.out.println(("spices / eaters") + "ложек(ки/ка) специй");
+
+       }
+ }
+
